ebad15f05ed963b0536a12dd1bf6ec80442e6a89
Andrey N <neonbones@icloud.com>
2022-06-16 00:02:39 +0300

    bugfix 2.0

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-let editButton = document.querySelector('.edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
-let saveButton = document.querySelector('.popup__button-save');
 let name = document.querySelector('.profile__name');
 let subname = document.querySelector('.profile__subname')
-let form = document.querySelector('.form')
+let form = document.querySelector('.popup__forms')
 let formName = document.querySelector('.popup__form_type_name');
 let formSubname = document.querySelector('.popup__form_type_subname');
 function inValue() {
@@ -28,13 +27,10 @@ closeButton.addEventListener('click', closeEditor);
 function formSubmitHandler(evt) {
     evt.preventDefault();
     let name = document.querySelector('.profile__name');
-    let subname = document.querySelector('.profile__subname')
-    name.textContent = `${formName.value}`
-    subname.textContent = `${formSubname
-        .value}`
-        popup
-        .classList
-        .remove('popup_active');
+    let subname = document.querySelector('.profile__subname');
+    name.textContent = formName.value;
+    subname.textContent = formSubname.value;
+    closeEditor();
 }
 form.addEventListener('submit', formSubmitHandler);
 
